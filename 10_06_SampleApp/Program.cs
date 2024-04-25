d95ad594b6254cea20a65bda70648ac7d309696a
jberthoty <jakub.berthoty@profinit.eu>
2024-04-25 16:42:29 +0200

    SampleApp for 10.06

---
+++ 10_06_SampleApp/Program.cs
@@ -0,0 +1,27 @@
+using Microsoft.ApplicationInsights.Extensibility;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddApplicationInsightsTelemetry();
+
+var app = builder.Build();
+
+app.MapGet("/", () => $"Good morning friend!");
+app.MapGet("/health", () => "Healthy");
+app.MapGet("/information", (string message) => app.Logger.LogInformation(message));
+app.MapGet(
+    "/error",
+    (string message) => {
+        throw new InvalidOperationException(message);
+    }
+);
+app.MapPost("/person", (SamplePersonData data) => Results.Created());
+
+app.Run();
+
+class SamplePersonData
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Address { get; set; } = string.Empty;
+}
